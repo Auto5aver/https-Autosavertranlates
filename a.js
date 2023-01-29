a529acc919104ec738a0329abdb2d422433a6d1f
Auto5aver <jonescentry@gmail.com>
2023-01-29 16:55:23 +1300

    Novel

---
+++ a.js
@@ -0,0 +1,13 @@
+document.getElementById("home-button").addEventListener("click", function(){
+  window.document.Webproject = "A.html";
+});
+
+document.getElementById("previous-button").addEventListener("click", function(){
+ window.document.Webproject.href = "A.html";
+
+});
+
+document.getElementById("next-button").addEventListener("click", function(){
+  window.document.Webproject = "B.html";
+
+});
